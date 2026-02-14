9d078b8b3a44af138f0cdc110fd693db1be2b140
RoamX <usmanbelloa@gmail.com>
2026-02-14 08:11:34 +0100

    contact added

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,4 +1,3 @@
-// src/components/Contact/index.jsx
 import { useEffect, useState } from 'react'
 import Loader from 'react-loaders'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
@@ -7,7 +6,7 @@ import emailjs from '@emailjs/browser'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 
-const Contact = () => {
+export default function Contact() {
   const [letterClass, setLetterClass] = useState('text-animate')
   const form = useRef()
 
@@ -41,77 +40,54 @@ const Contact = () => {
   return (
     <>
       <div className="container contact-page">
-        <div className="text-zone">
-          <h1>
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
-              idx={15}
-            />
-          </h1>
-          <p>
-            I am interested in freelance opportunities - especially ambitious or
-            large projects. However, if you have other request or question,
-            don't hesitate to contact me using below form either.
-          </p>
-          <div className="contact-form">
-            <form ref={form} onSubmit={sendEmail}>
-              <ul>
-                <li className="half">
+        <div className="flex-layout">
+          <div className="text-zone">
+            <h1>
+              <AnimatedLetters
+                letterClass={letterClass}
+                strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+                idx={15}
+              />
+            </h1>
+            <p>
+              I am open to infrastructure architecture roles, Web3 middleware 
+              contracts, or freelance engineering. If you have questions about 
+              gasless UX or node automation, drop a message below.
+            </p>
+            <div className="contact-form">
+              <form ref={form} onSubmit={sendEmail}>
+                <div className="form-group-half">
                   <input placeholder="Name" type="text" name="name" required />
-                </li>
-                <li className="half">
-                  <input
-                    placeholder="Email"
-                    type="email"
-                    name="email"
-                    required
-                  />
-                </li>
-                <li>
-                  <input
-                    placeholder="Subject"
-                    type="text"
-                    name="subject"
-                    required
-                  />
-                </li>
-                <li>
-                  <textarea
-                    placeholder="Message"
-                    name="message"
-                    required
-                  ></textarea>
-                </li>
-                <li>
-                  <input type="submit" className="flat-button" value="SEND" />
-                </li>
-              </ul>
-            </form>
+                  <input placeholder="Email" type="email" name="email" required />
+                </div>
+                <input placeholder="Subject" type="text" name="subject" required />
+                <textarea placeholder="Message" name="message" required></textarea>
+                <button type="submit" className="flat-button">SEND MESSAGE</button>
+              </form>
+            </div>
           </div>
-        </div>
-        <div className="info-map">
-          Bello Usman Abdullahi,
-          <br />
-          Nigeria,
-          <br />
-          D4 Duwa plaza Opp Bachi park, <br />
-          GRA Gombe. <br />
-          <br />
-          <span>usmanbelloa@gmail.com</span>
-        </div>
-        <div className="map-wrap">
-          <MapContainer center={[10.283333, 11.166667]} zoom={10}>
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            <Marker position={[10.283333, 11.166667]}>
-              <Popup>Bello Usman lives here, come over for a cup of coffee :)</Popup>
-            </Marker>
-          </MapContainer>
+          <div className="map-zone">
+            <div className="info-map">
+              Bello Usman Abdullahi,<br />
+              Nigeria,
+              <br />
+              CCECC lab, <br />
+              Idu industrial, Abuja <br />
+              <br />
+              <span>usmanbelloa@gmail.com</span>
+            </div>
+            <div className="map-wrap">
+              <MapContainer center={[10.283333, 11.166667]} zoom={10}>
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                <Marker position={[10.283333, 11.166667]}>
+                  <Popup>Bello Usman lives here, come over for a cup of coffee :)</Popup>
+                </Marker>
+              </MapContainer>
+            </div>
+          </div>
         </div>
       </div>
       <Loader type="pacman" />
     </>
   )
 }
-
-export default Contact
